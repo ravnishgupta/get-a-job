608276a70949fd103ebca9f0a8606b9f0109e626
meghark <98137756+meghark@users.noreply.github.com>
2022-07-05 14:00:02 -0700

    Render projects screens and skills list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,18 +11,58 @@ const Home = () => {
   const projects = data?.projects || [];
   console.log(projects);
 
+  const prSample = [{
+    _id : 1,
+    title: "Test projects",
+    description : "As we shared in the release post, React 18 introduces features powered by our new concurrent renderer, with a gradual adoption strategy for existing applications. In this post, we will guide you through the steps for upgrading to React 18.",
+    startDate: "2022-07-31",
+    endDate:"2023-07-31",
+    payPerHour:"80",
+    skills: ["C#", "JS", ".NET", "SQL Server"]
+  },
+  {
+    _id : 2,
+    title: "Test projects2",
+    description : "Test project 2",
+    startDate: "2022-07-31",
+    endDate:"2023-07-31",
+    payPerHour:"80",
+    skills: ["C#", "JS", ".NET", "SQL Server"]
+  },
+  {
+    _id : 3,
+    title: "Test projects3",
+    description : "Test project 3",
+    startDate: "2022-07-31",
+    endDate:"2023-07-31",
+    payPerHour:"80",
+    skills: ["C#", "JS", ".NET", "SQL Server"]
+  }]
+
   return (
-    <main>
-    <div className="flex-row justify-space-between">
-      <div className="col-12 mb-3">
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ProjectList projects={projects} title="Projects" />
-        )}
-      </div>
+    <div className="bg-white">
+    <header >
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Home</h1>
+        </div>
+    </header>
+    <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <section aria-labelledby="talents-heading" className="pt-6 pb-24">
+        <h2 id="talents-heading" className="sr-only">Projects</h2>
+
+           
+            <div className="col-12 mb-3">
+            {loading ? (
+              <div>Loading...</div>
+            ) : ( 
+              <ProjectList projects={prSample} title="Projects" />
+           
+            )}
+          
+        </div>
+      </section>
+    </main>
     </div>
-  </main>
   );
 };
 
