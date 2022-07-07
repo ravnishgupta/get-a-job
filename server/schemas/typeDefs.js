09a79cf688cbff97d5f6ad780e5a8b1437804791
nitishd22 <nitishd@gmail.com>
2022-07-07 16:01:35 -0700

    fields updates

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,9 +4,12 @@ const typeDefs = gql`
 
 type User {
     _id: ID
-    username: String
     email: String
     applications: String
+    password: String
+    availableNow: Boolean
+    hourlyRate: Number
+    github: String
 }
 
 type Projects {
@@ -14,6 +17,12 @@ type Projects {
     description: String
     title: String
 }
+
+type Skills {
+  _id : ID
+  description: String
+}
+
 type Auth {
     token: ID!
     user: User
@@ -25,7 +34,8 @@ type Query {
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(email: String!, password: String!): Auth
+    saveProject(description: String!, title: String!): Auth
   }
   
   `
